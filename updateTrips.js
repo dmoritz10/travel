aee9e20ebb18ca0921833c951e6ff440b3454d65
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-31 11:43:57 -0700

    dan

--- updateTrips.js
+++ updateTrips.js
@@ -7,64 +7,64 @@ async function updateTrips() {
 
         ])
     
-        var valsLHD    = objShts['Location History Detail'].vals
-        var hdrsLHD   = objShts['Location History Detail'].colHdrs
-      
-        var valsTRP  = objShts['Trips'].vals
-        var hdrsTRP = objShts['Trips'].colHdrs
-     
-        const LHDTripCol = hdrsLHD.indexOf('Trip')
+    var valsLHD    = objShts['Location History Detail'].vals
+    var hdrsLHD   = objShts['Location History Detail'].colHdrs
+  
+    var valsTRP  = objShts['Trips'].vals
+    var hdrsTRP = objShts['Trips'].colHdrs
+  
+    const LHDTripCol = hdrsLHD.indexOf('Trip')
 
-        const TRPTripCol = hdrsTRP.indexOf('Trip')
-        const colTRPTrips = valsTRP.map(x => x[TRPTripCol]);
-      
-        var updateCntr = 0
-        var appendCntr = 0
-      
+    const TRPTripCol = hdrsTRP.indexOf('Trip')
+    const colTRPTrips = valsTRP.map(x => x[TRPTripCol]);
+  
+    var updateCntr = 0
+    var appendCntr = 0
+  
 
-        for (var i = 0;i<valsLHD.length;i++) {
-      
-          var ele = valsLHD[i]
+    for (var i = 0;i<valsLHD.length;i++) {
+  
+      var ele = valsLHD[i]
 
-          if (ele[LHDTripCol])
-            {
-              var trip = buildTrip(i, valsLHD, hdrsLHD, valsTRP, hdrsTRP)
-              i = trip.endIdx*1 + 1
+      if (ele[LHDTripCol])
+        {
+          var trip = buildTrip(i, valsLHD, hdrsLHD, valsTRP, hdrsTRP)
+          i = trip.endIdx*1 + 1
 
-            } else continue
-          
-            
+        } else continue
+      
+        
 
-          let row = colTRPTrips.indexOf(ele[LHDTripCol])
-          
-          if (row == -1)   {
+      let row = colTRPTrips.indexOf(ele[LHDTripCol])
+      
+      if (row == -1)   {
 
-            valsTRP.push(trip.val);
-            appendCntr++
-          } else {
-            // if (trp[hdrsTRP.indexOf('Source')] != 'Manual') {
-              valsTRP[row] = trip.val;
-              updateCntr++
-            // }
-          }
-      
-        }
-      
-        console.log('updateAppendSht')
-        console.log('updateCntr', updateCntr)
-        console.log('appendCntr', appendCntr)
-        console.log('total', appendCntr + updateCntr)
-      
-        var shtArr = [hdrsTRP].concat(valsTRP)
-      
-        console.time('updateSheet')
-      
-        await updateSheet('Trips', shtArr)
-      
-        console.timeEnd('updateSheet')
-      
-        console.log('Update complete')
-      
+        valsTRP.push(trip.val);
+        appendCntr++
+      } else {
+        // if (trp[hdrsTRP.indexOf('Source')] != 'Manual') {
+          valsTRP[row] = trip.val;
+          updateCntr++
+        // }
+      }
+  
+    }
+  
+    console.log('updateAppendSht')
+    console.log('updateCntr', updateCntr)
+    console.log('appendCntr', appendCntr)
+    console.log('total', appendCntr + updateCntr)
+  
+    var shtArr = [hdrsTRP].concat(valsTRP)
+  
+    console.time('updateSheet')
+  
+    await updateSheet('Trips', shtArr)
+  
+    console.timeEnd('updateSheet')
+  
+    console.log('Update complete')
+  
 }
 
 function buildTrip(strIdx, valsLHD, hdrsLHD, valsTRP, hdrsTRP) {
@@ -74,7 +74,7 @@ function buildTrip(strIdx, valsLHD, hdrsLHD, valsTRP, hdrsTRP) {
 
   trp[hdrsTRP.indexOf('Trip')]           = ele[hdrsLHD.indexOf('Trip')]
   trp[hdrsTRP.indexOf('Month')]          = ele[hdrsLHD.indexOf('Month')]
-  trp[hdrsTRP.indexOf('Destinations')]   = ele[hdrsLHD.indexOf('Trip')].substring(0, ele[hdrsLHD.indexOf('Trip')].length - 11)
+  trp[hdrsTRP.indexOf('Destinations')]   = ele[hdrsLHD.indexOf('Trip')].slice(0, -11)
   trp[hdrsTRP.indexOf('Type')]           = ''
   trp[hdrsTRP.indexOf('Start Date')]     = ele[hdrsLHD.indexOf('Date')].split(',')[0]
   trp[hdrsTRP.indexOf('Source')]         = 'LHD'
