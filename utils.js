f159192981004e7f5c469416d6ae94e009ffca5e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-11 11:25:01 -0700

    dan

--- utils.js
+++ utils.js
@@ -685,3 +685,28 @@ function formatDateTime(d, t) {
 
 }
 
+function parseMonth(mo) {
+
+  const moNbr = new Map([
+    ['Jan', '01'],
+    ['Feb', '02'],
+    ['Mar', '03'],
+    ['Apr', '04'],
+    ['May', '05'],
+    ['Jun', '06'],
+    ['Jul', '07'],
+    ['Aug', '08'],
+    ['Sep', '09'],
+    ['Oct', '10'],
+    ['Nov', '11'],
+    ['Dec', '12']
+  ])
+
+  var d = mo.split(' ')
+  var yr = d[1]
+
+  var mo =moNbr.get(d[0])
+
+  return yr + '-' + mo
+
+}
