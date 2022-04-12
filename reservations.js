ca74a6e1e59fa63081b206514c622941a35478cc
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-12 08:37:22 -0700

    dan

--- reservations.js
+++ reservations.js
@@ -4,9 +4,10 @@ async function importFromCalendar() {
     var resource = {
   
         'calendarId': 'primary',
-        'maxResults': '1000',
+        'maxResults': 1000,
+        'orderBy': 'startTime',
         'timeMin': '2021-11-01T10:00:00-07:00',
-        'timeMax': '2022-02-01T10:00:00-07:00',
+        'timeMax': '2022-02-01T10:00:00-07:00'
 
      };
        
