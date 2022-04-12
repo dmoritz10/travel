6e8da2de145d961ded5dacd53d44f9d80b445987
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-12 09:25:58 -0700

    dan

--- reservations.js
+++ reservations.js
@@ -1,16 +1,10 @@
 async function importFromCalendar() {
 
 
-    var resource = {
-  
-        'calendarId': 'primary',
-        'maxResults': 1000,
-        'orderBy': 'startTime',
-        'timeMin': '2021-11-01T10:00:00-07:00',
-        'timeMax': '2022-02-01T10:00:00-07:00'
-
-     };
-       
+    
+        var timeMin = '2021-11-01T10:00:00-07:00'
+        var timeMax = '2022-02-01T10:00:00-07:00'
+   
       await checkAuth()
       var request = await gapi.client.calendar.events.list({
           
@@ -18,8 +12,27 @@ async function importFromCalendar() {
         'maxResults': 1000,
         'singleEvents': true,
         'orderBy': 'startTime',
-        'timeMin': '2022-05-19T10:00:00-07:00',
-        'timeMax': '2022-05-30T10:00:00-07:00'
+        'timeMin': timeMin,
+        'timeMax': timeMax
+        
+      });
+  
+      var eventId = request.result.id
+
+      console.log(request.result)
+  
+      var timeMin = '2022-04-04T10:00:00-07:00'
+      var timeMax = '2022-04-08T10:00:00-07:00'
+
+      await checkAuth()
+      var request = await gapi.client.calendar.events.list({
+          
+        'calendarId': 'primary',
+        'maxResults': 1000,
+        'singleEvents': true,
+        'orderBy': 'startTime',
+        'timeMin': timeMin,
+        'timeMax': timeMax
         
       });
   
