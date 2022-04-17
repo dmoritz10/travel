a2fd6b88e4ca57ebb30cadad2ae161d4e2bf0c75
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-17 08:31:06 -0700

    dan

--- places.js
+++ places.js
@@ -1,55 +1,84 @@
-function btnPlacesHtml() {
+async function btnPlacesHtml() {
+
+    modal(true)
+
+    var plOptions = readOption('resFilter')
+    var plSelectFav = resOptions.resSelectFav
+  
+    var objSht = await openShts(
+      [
+        { title: "Reservations", type: "all" }
+      ])
+  
+    console.log('objSht', objSht)
+  
+    resTitle = "Reservations"
+    
+    resHdrs = objSht[resTitle].colHdrs
+    resVals = objSht[resTitle].vals
+    
+  var keyCol = resHdrs.indexOf('Composite Key')
+  
+  var vals = resVals
+
+    var arr = []
+  
+    for (var i=0;i<vals.length;i++) {
+  
+        var key = vals[i][keyCol]
+
+        var countries = key.split(' | ')
+        var x = key.split(' - ')
+    
+        var trip = x[0]
+        var month = x[1]
+    
+        countries.forEach(ele => {
+    
+            arr.push([ele, trip, month])
+    
+        })
+      
+    }
+  
+    arr.sort(function(a,b){return a[0] > b[0] ? 1 : -1; });
+    
+    var treeData = []
+    var brkcntry
+
+    arr.forEach( ele => {
+
+        var cntry = ele[0]
+        var trip = ele[1]
+        var month = ele[2]
+
+        if (!brkcntry) 
+
+        if (brkcntry != cntry) {
 
 
+            if (brkcntry) treeData.push(cntryObj)
 
+            brkcntry = cntry
 
-    var treeData = [
-        {
-          text: "Node 1",
-          icon: "fa fa-folder", // requires font awesome
-          nodes: [
-            {
-              text: "Sub Node 1",
-              icon: "fa fa-folder",
-              nodes: [
-                {
-                  id:    "sub-node-1",
-                  text:  "Sub Child Node 1",
-                  icon:  "fa fa-folder",
-                  class: "nav-level-3",
-                  href:  " "
-                },
-                {
-                  text: "Sub Child Node 2",
-                  icon: "fa fa-folder"
-                }
-              ]
-            },
-            {
-              text: "Sub Node 2",
-               icon: "fa fa-folder"
+            var cntryObj = {
+
+                text: cntry,
+                nodes: []
+
             }
-          ]
-        },
-        {
-          text: "Node 2",
-          icon: "fa fa-folder"
-        },
-        {
-          text: "Node 3",
-          icon: "fa fa-folder"
-        },
-        {
-          text: "Node 4",
-          icon: "fa fa-folder"
-        },
-        {
-          text: "Node 5",
-          icon: "fa fa-folder"
+
         }
-    ];
 
+        cntryObj.nodes.push({
 
+            text: month + ' - ' + trip
+
+        })
+
+    })
+
+    console.log(cntryObj)
 
     $('#plContainer').bstreeview({ 
         data: treeData
