5d8badd27d5e463d3f9d9292f44dab2670481e58
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-21 07:14:38 -0700

    dan

--- importPlaces.js
+++ importPlaces.js
@@ -649,7 +649,7 @@ trp[hdrsTRP.indexOf('Trip')]           = ele[hdrsLHD.indexOf('Trip')]
 trp[hdrsTRP.indexOf('Month')]          = ele[hdrsLHD.indexOf('Month')]
 trp[hdrsTRP.indexOf('Destinations')]   = ele[hdrsLHD.indexOf('Destinations')]
 trp[hdrsTRP.indexOf('Countries')]      = JSON.stringify(cntry)
-trp[hdrsTRP.indexOf('Type')]           = ele[hdrsLHD.indexOf('Country')].indexOf('USA') > -1 ? "Domestic" : "International"
+trp[hdrsTRP.indexOf('Type')]           = cntries.length == 1 && cntries[0] == "USA" ? "Domestic" : "International"
 trp[hdrsTRP.indexOf('Start Date')]     = ele[hdrsLHD.indexOf('Date')].split(',')[0]
 trp[hdrsTRP.indexOf('Source')]         = 'LHD'
 
