9db550715893649a657c518433c9b596cddf3d41
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-04-27 08:42:49 -0700

    dan

--- reservations.js
+++ reservations.js
@@ -708,34 +708,19 @@ function btnShowCalendarHtml() {
 
       // header controls
       header: {
-        left: 'dayGridMonth,timeGridWeek,timeGridDay custom1',
+        left: 'dayGridMonth,timeGridWeek,timeGridDay',
         center: 'title',
-        right: 'custom2 prevYear,prev,next,nextYear' 
+        right: 'prevYear,prev,next,nextYear' 
       },
 
       // footer controls
-      footer: {
-        left: 'custom1,custom2',
-        center: '',
-        right: 'prev,next' 
-      },
+      // footer: {
+      //   center: '',
+      //   right: 'prev,next' 
+      // },
     
     // custom toolbar buttons
-    customButtons: {
-      custom1: {
-        text: 'custom 1',
-        click: function () {
-          alert('clicked custom button 1!');
-        } 
-      },
-
-      custom2: {
-        text: 'custom 2',
-        click: function () {
-          alert('clicked custom button 2!');
-        }
-      } 
-    },
+    
     events: [
       {
         title: 'All Day Event',
