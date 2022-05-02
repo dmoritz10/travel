ce38108e560ee91e7a30cbd33f018b5ed3d9e078
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-05-02 10:17:16 -0700

    dan

--- tripsByYr.js
+++ tripsByYr.js
@@ -38,7 +38,7 @@ async function btnTripByYrHtml() {
     
   
     var trp = [["", "J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"]]
-    var circle = '<span class="material-icons" style="color:#cccc00;">circle</span>'
+    var circle = '<span class="material-icons" style="fontSize: 5;color:#cccc00;">circle</span>'
 
     for (var j = 0; j < trpVals.length; j++) {
 
