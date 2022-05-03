7aa4ed0d5fad80066896e8f72a86f34e88362d27
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-05-03 13:33:40 -0700

    dan

--- tripsByYr.js
+++ tripsByYr.js
@@ -67,7 +67,7 @@ async function btnTripByYrHtml() {
         var vals = trpVals[i]
 
         var sDate = vals[trpHdrs.indexOf("Start Date")]
-        var eDate = vals[trpHdrs.indexOf("End Date")
+        var eDate = vals[trpHdrs.indexOf("End Date")]
         var trip = vals[trpHdrs.indexOf("Trip")]
     ]
 
