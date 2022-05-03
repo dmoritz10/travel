68e83a97a0dd3c64598799bfe4459cf82129c7cc
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-05-03 08:48:55 -0700

    dan

--- tripsByYr.js
+++ tripsByYr.js
@@ -145,7 +145,7 @@ function clearSpuriousDots(trp, firstOfYr) {
         for (var j = lastDyOfMo + 1; j<32;j++) {
 
             var col = mo
-            var row = j + 1
+            var row = j
 
             console.log('innerloop', col, row)
 
