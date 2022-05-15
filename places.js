b8fb2260c5fbe6d2755f7b54f3053de3e3f982df
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-05-14 17:18:03 -0700

    dan

--- places.js
+++ places.js
@@ -62,7 +62,8 @@ async function btnPlacesHtml() {
     var treeData = []
     var brkName
 
-    for (var i=0;i<arr.length;i++) {
+    // for (var i=0;i<arr.length;i++) {
+        for (var i=0;i<20;i++) {
 
         var ele = arr[i]
 
