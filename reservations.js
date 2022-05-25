e087f4a9de4ec8de9346ec7073a159755ec322f5
dmoritz10 <dmoritz10@gmail.com>
2022-05-25 02:39:19 -0700

    dan

--- reservations.js
+++ reservations.js
@@ -85,10 +85,10 @@ async function listReservations(title = "Reservations") {
     ele.find('#resType')[0].innerHTML = resObj['Type']
     ele.find('#resStartEndDateTime')[0].innerHTML = start + (end ? (' - ' + end) : '')
     ele.find('#resStatus')[0].innerHTML = resObj['Status']
-    ele.find('#resLocation')[0].innerHTML = locnHtml
+    ele.find('#resLocation')[0].innerHTML = locnHtml ? locnHtml : ele.find('#resLocation').addClass('d-none')
     ele.find('#resConfirmation')[0].innerHTML = resObj['Confirmation'] ? 'Confirmation: ' + resObj['Confirmation'] : ele.find('#resConfirmation').addClass('d-none')
-    ele.find('#resPhone')[0].innerHTML = resObj['Phone'] ? 'Phone: ' + resObj['Phone'] : ''
-    ele.find('#resDescription')[0].innerHTML = resObj['Description'] ? resObj['Description'].replace(/\n/g, "<br>") : ''
+    ele.find('#resPhone')[0].innerHTML = resObj['Phone'] ? 'Phone: ' + resObj['Phone'] : ele.find('#resPhone').addClass('d-none')
+    ele.find('#resDescription')[0].innerHTML = resObj['Description'] ? resObj['Description'].replace(/\n/g, "<br>") : ele.find('#resDescription').addClass('d-none')
 
     ele.find('#btnResEdit')[0].setAttribute("onclick", "editReservation(" + j + ")");
 
