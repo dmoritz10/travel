594966c753b98fc3cdbf7d29a88f5795be330607
dmoritz10 <dmoritz10@gmail.com>
2022-06-06 00:24:04 -0700

    dan

--- show trip.js
+++ show trip.js
@@ -26,8 +26,9 @@ async function showTrip(idx) {
     if (date != brkDate) {
 
       var dispDate = DateTime.fromJSDate(new Date(date)).toFormat('ccc L/d');
+      var hrefDate = DateTime.fromJSDate(new Date(date)).toFormat('yyyy-LL-dd');
 
-      var googleTimelineHref = 'https://timeline.google.com/maps/timeline?pb=!1m2!1m1!1s' + date
+      var googleTimelineHref = 'https://timeline.google.com/maps/timeline?pb=!1m2!1m1!1s' + hrefDate
 
       console.log('googleTimelineHref', googleTimelineHref)
 
