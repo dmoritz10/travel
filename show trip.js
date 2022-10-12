67cdade3a05323cfa278b5b7db9b374802a50ea1
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-10-12 14:13:17 -0700

    dan

--- show trip.js
+++ show trip.js
@@ -38,7 +38,6 @@ async function showTrip(idx) {
         var hrefDate = DateTime.fromJSDate(new Date(brkDate)).toFormat('yyyy-LL-dd');
         var googleTimelineHref = 'https://timeline.google.com/maps/timeline?pb=!1m2!1m1!1s' + hrefDate
         trp[priorHdr][1] = trp[priorHdr][1].replace(/replacementToken/g, actDisp).replace(/hrefDateToken/g, googleTimelineHref)
-        console.log('trp', trp[priorHdr][1])
     
       }
       
@@ -69,8 +68,16 @@ async function showTrip(idx) {
   }
 
   var actDisp = formatActivities(activities)
-  if (priorHdr > -1) trp[priorHdr][1] = trp[priorHdr][1].replace(/replacementToken/g, actDisp).replace(/hrefDateToken/g, googleTimelineHref)
+  if (priorHdr > -1) 
 
+    {
+
+      var hrefDate = DateTime.fromJSDate(new Date(date)).toFormat('yyyy-LL-dd');
+      var googleTimelineHref = 'https://timeline.google.com/maps/timeline?pb=!1m2!1m1!1s' + hrefDate
+      trp[priorHdr][1] = trp[priorHdr][1].replace(/replacementToken/g, actDisp).replace(/hrefDateToken/g, googleTimelineHref)
+
+    }
+  
   
   var tbl = new Table();
   
