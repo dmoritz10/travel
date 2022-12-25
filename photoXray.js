ef0e879ca7066e91cdf7e5dbfd83eab761ae55f1
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-12-25 14:11:50 -0800

    dan

--- photoXray.js
+++ photoXray.js
@@ -164,8 +164,8 @@ function calcTrip(picDate) {
         sDate:      trp[strCol],
         eDate:      trp[endCol],
         month:      trp[monthCol],
-        dest:       trp[destCol].replace(/\[/g, '').replace(/\]/g, '').replace(/,/g, ''),
-        countries:  trp[countriesCol].replace(/\[/g, '').replace(/\]/g, '').replace(/,/g, '')
+        dest:       trp[destCol].slice(1).slice(0, -1).replace(/,/g, ''),
+        countries:  trp[countriesCol].slice(1).slice(0, -1).replace(/,/g, '')
 
       }
 
