7d6c66b0b8b32cfdbfee95b0df4423e59eed7bd8
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-01-15 08:32:09 -0800

    dan

--- countries.js
+++ countries.js
@@ -144,6 +144,8 @@ async function btnCountriesHtml() {
 
     };
 
+    await loadMap()
+
       modal(false)
       gotoTab("Countries")
 }
@@ -165,4 +167,77 @@ function countrySorter(firstKey, secondKey) {
             }
         } 
     }  
+}
+
+async function loadMap() {
+
+    const topology = await fetch(
+        'https://code.highcharts.com/mapdata/custom/world.topo.json'
+    ).then(response => response.json());
+
+    Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population-density.json', function (data) {
+
+        console.log('data', data)
+
+        // Prevent logarithmic errors in color calulcation
+        data.forEach(function (p) {
+            p.value = (p.value < 1 ? 1 : p.value);
+        });
+
+        // Initialize the chart
+        Highcharts.mapChart('container', {
+            chart: {
+                map: topology
+            },
+
+            title: {
+                text: 'Zoom in on country by double click'
+            },
+
+            mapNavigation: {
+                enabled: true,
+                enableDoubleClickZoomTo: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+
+            mapView: {
+                fitToGeometry: {
+                    type: 'MultiPoint',
+                    coordinates: [
+                        // Alaska west
+                        [-164, 54],
+                        // Greenland north
+                        [-35, 84],
+                        // New Zealand east
+                        [179, -38],
+                        // Chile south
+                        [-68, -55]
+                    ]
+                }
+            },
+
+            colorAxis: {
+                min: 1,
+                max: 1000,
+                type: 'logarithmic'
+            },
+
+            series: [{
+                data: data,
+                joinBy: ['iso-a3', 'code3'],
+                name: 'Population density',
+                states: {
+                    hover: {
+                        color: '#a4edba'
+                    }
+                },
+                tooltip: {
+                    valueSuffix: '/km²'
+                }
+            }]
+        });
+    });
+
 }
