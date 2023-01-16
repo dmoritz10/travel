a7cc0c38fe8b5017497afdfc3e3102082c87b404
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-01-16 08:36:22 -0800

    dan

--- countries.js
+++ countries.js
@@ -195,7 +195,7 @@ async function loadMap(cntryCnt) {
       }
 
     const topology = await fetch(
-        'https://code.highcharts.com/mapdata/custom/world-highres2.js'
+        'https://code.highcharts.com/mapdata/custom/world.topo.json'
     ).then(response => response.json());
 
 
