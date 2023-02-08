04c67cd2e6130d2f5b686dcb2e900e3965ebeb12
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-02-08 10:07:32 -0800

    dan

--- home-js.js
+++ home-js.js
@@ -1,11 +1,11 @@
 
 async function goHome() {
 
-  var signinStatus = await gapi.auth2.getAuthInstance().isSignedIn.get()
-   if (!signinStatus) {
-     gotoTab('Auth')
-     return
-   }
+  // var signinStatus = await gapi.auth2.getAuthInstance().isSignedIn.get()
+  //  if (!signinStatus) {
+  //    gotoTab('Auth')
+  //    return
+  //  }
   
   gotoTab('Home')
 
