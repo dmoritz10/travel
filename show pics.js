e923238c86ae9e886688580a494d4e0f7369b77e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-04-30 11:57:11 -0700

    dan

--- show pics.js
+++ show pics.js
@@ -139,12 +139,12 @@ async function showPics(idx, rtnToPage='Trips') {
                 "startDate": {
                     "year": 2022,
                     "month": 06,
-                    "day": 09
+                    "day": 30
                 },
                 "endDate": {
                     "year": 2022,
                     "month": 06,
-                    "day": 12
+                    "day": 01
                 }
             }
             ]
