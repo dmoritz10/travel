686169a4eab320a96c02f7d40566db11124ec651
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-04-30 10:18:18 -0700

    dan

--- show pics.js
+++ show pics.js
@@ -162,5 +162,229 @@ async function showPics(idx, rtnToPage='Trips') {
 
     console.log('response', response)
 
+    let element= await embed_google_media('L74MSFRNuyNSmrKm9', 'L74MSFRNuyNSmrKm9', 'carousel');
+
+}
+
+async function embed_google_media(sharable_id, id ,  type='grid' , proxy_function = _make_request , height = 240, ){
+ 
+    const album_url = `https://photos.app.goo.gl/${sharable_id.trim()}`
+
+    
+    let json =await proxy_function(album_url)
+
+    var html = json.body.data
+
+    let container = document.getElementById(id)
+    var re = /src="(.{0,1000})=w/g;
+    
+    var urls = Array.from(html.matchAll(re)).map(match => match[1]) //find all links in the html that matter get the group we found
+    var media = await Promise.all(urls.map(url => _url_to_media_item(url,proxy_function))) // make array by mapping urls using the url to media item function
+    
+    if (type=='grid'){
+        return _elements_to_grid(media, container, height)
+    // } else if (type=='carousel') {
+    //     return _element_to_carousel(media, container)
+    }
+}   
+
+async function  _url_to_media_item(url,proxy_function){
+    let media_item = {}
+    media_item.base_url = url
+    
+    // try seeing if this media item is a video 
+    let json = await proxy_function(`${url}=dv`) 
+    
+    if (json.body.redirected){ // if this is a success then the object is a video / moving picture if it fails then it is not
+        media_item.type = 'video'
+    } else { //media is a image
+        media_item.type = 'image'
+    }
+    media_item.aspect_ratio = await _get_aspect_ratio(`${url}=w20-h20`)
+    return media_item
+}
+
+function _get_aspect_ratio(url) {
+    return new Promise((resolve, reject) => {
+        let img = new Image();
+        img.onload = () => resolve(img.naturalWidth/img.naturalHeight);
+        img.onerror = () => reject();
+        img.src = url;
+
+    });
+}
+
+function _elements_to_grid(media, container, max_row_height){
+    container.classList.add('photo-grid') 
+    MediaGrid(container,media,max_row_height)
+}
+
+// function _element_to_carousel(media,container) {
+//     container.classList.add('photo-carousel')
+//     MediaCarousel(container,media)
+// }
+
+
+async function _make_request(url){
+    var response = await fetch(`${proxy_url}${encodeURIComponent(url)}`) // get request the album page
+        
+    var json = await response.json() // get html  
+
+    return json 
 }
   
+function MediaGrid(container, media, max_height) {
+    var space_between_media = 4;
+
+    let media_objects = media.map(item => new MediaObject(item));
+    media_objects.forEach(element => _add_object_to_container(element));
+
+    window.onresize= () => _calc_rows();
+
+    let timer = window.setInterval(() => {
+        if(container.clientWidth>0){
+            _calc_rows()
+            _kill_timer()
+        }
+    },100)
+
+    function _kill_timer(){
+        window.clearInterval(timer)
+    }
+
+    function _calc_rows() {
+        let min_aspect = _get_min_aspect_ratio()
+    
+        let rows = []
+        let current_aspect_ratio = 0
+        let current_row = []
+        for(let object of media_objects){
+            current_aspect_ratio += object.aspect_ratio
+            current_row.push(object)
+            if (current_aspect_ratio > min_aspect){ // this row is full we can cap it
+                let row_item  = {}
+                row_item.items = current_row
+                row_item.aspect_ratio = current_aspect_ratio
+                rows.push(row_item)
+                // resetting the temp loop vars 
+                current_aspect_ratio = 0 
+                current_row = []
+            } 
+        }
+        // check if theres photos left that dont make a whole row
+
+        if(current_aspect_ratio > 0) {
+            let row_item  = {}
+            row_item.items = current_row
+            row_item.aspect_ratio = min_aspect
+            rows.push(row_item)
+        }
+
+        _populate_container(rows)  
+    }
+
+
+    function _populate_container(rows) {
+        let x = 0
+        let y = 0 
+        for (let row of rows){
+            let image_width = container.clientWidth - space_between_media * (row.items.length - 1);
+            let row_height = image_width / row.aspect_ratio;
+            for (let object of row.items) {
+                object._update_position(x,y,row_height);
+                x = x+ object.aspect_ratio*row_height + space_between_media
+            }
+            x = 0 
+            y = y + row_height + space_between_media
+        }
+
+        container.style = `height:${y}px`
+        
+    }
+
+    function _add_object_to_container(object) {
+        container.appendChild(object.dom_object)
+
+    }
+
+    // dynamically calculates the min aspect ration needed to fill the screen based on the desired max_height for images
+    function _get_min_aspect_ratio() {
+        let width = container.clientWidth;
+        return width/max_height;
+    }
+   
+}
+
+class MediaObject{
+    constructor(item){
+        this.base_url = item.base_url
+        this.type = item.type
+        this.aspect_ratio = item.aspect_ratio
+
+        this.dom_object = document.createElement('div')
+        this.dom_object.classList.add('media-object')
+        this.dom_object.style = `width:${this.aspect_ratio*20}px;height:${20}px`
+        this._fill_object()
+    }
+
+    _fill_object() {
+        // this.content = new Image();
+        // this.content.src = this._get_src_url(20)
+
+
+        if (this.type == 'image') {
+            let full_content = new Image();
+            full_content.src = this._get_src_url();
+            this.content = full_content
+            full_content.onload = this._replace_content;
+
+        }else if (this.type == 'video') {
+            let full_content = document.createElement('video')
+            full_content.src = this._get_src_url();
+            full_content.autoplay = true
+            full_content.loop = true
+            full_content.playsinline = true
+            full_content.muted = true
+            full_content.poster = this._get_src_url(null, true);
+            this.content = full_content
+            full_content.onloadeddata = this._replace_content;
+            full_content.onerror = (e) => e.target.load()
+        }
+
+        this.content.classList.add('blur')
+        this.dom_object.appendChild(this.content)
+    }
+
+
+
+    _replace_content() {
+        this.classList.remove('blur')
+    }
+
+
+    _update_position(x,y,height) {
+        
+
+        this.dom_object.style = `width:${parseInt(this.aspect_ratio*height)}px;height:${parseInt(height)}px;transform:translate(${parseInt(x)}px,${parseInt(y)}px)`
+    }
+
+    _get_src_url(size=null,poster=false){
+        let url = this.base_url + '='
+        if(poster){
+            url = url + 'd-no'
+        } else if (size == null) { // get the full size thing
+            url = url +'d'
+            if (this.type == 'video') {
+                url = url+'v'
+            }
+        } else {
+            url = `${url}w${size}-h${size}`
+            if (this.type == 'video') {
+                url = url + '-no'
+            }
+        }
+
+        return url
+    }
+}
+
