5631e673ad723c0f469942e646fe274a2e008d15
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-06 14:15:54 -0700

    dan

--- show pics.js
+++ show pics.js
@@ -66,7 +66,7 @@ async function showPics(idx) {
                 },
                 "contentFilter": {
                     "includedContentCategories": [
-                      [category]
+                        category
                     ]
                   }
             }
