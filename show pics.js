33ceddafd1994cf9e9f5cebbf203db46d8746be4
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-05-07 09:33:38 -0700

    dan

--- show pics.js
+++ show pics.js
@@ -142,7 +142,7 @@ async function showPics(idx) {
     }
 
     if (medArr.length>0) {
-        ele.find('#trppDate')[0].innerHTML = DateTime.fromISO(media.mediaMetadata.creationTime).toFormat('ccc L/d')
+        ele.find('#trppDate')[0].innerHTML = DateTime.fromISO(prevDte).toFormat('ccc L/d')
         await embed_google_media(medArr, ele.find('#trppPhotos')[0], 'grid');
         medArr = []
         ele.removeClass('d-none');
