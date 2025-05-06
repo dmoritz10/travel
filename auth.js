4bc4b96c7a3571b3fc7c806fbd2ae6c415f36007
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2025-05-06 10:27:53 -0700

    photos permissions

--- auth.js
+++ auth.js
@@ -1,12 +1,13 @@
-
-
-const API_KEY   = 'AIzaSyCObS1ZM8aAyPfqXZDtq2-rRrMqpJZxBc0'  
-const CLI_ID    = '8803561872-jd3c7f4e1ugeld0l6ssfmse40n5nfr6l.apps.googleusercontent.com'  
-const SCOPES    = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/photoslibrary'; // Space delimited if more than one
-const DISCOVERY = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 
-                  'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
-                  'https://www.googleapis.com/discovery/v1/apis/photoslibrary/v1/rest'
-                  ];
+const API_KEY = "AIzaSyCObS1ZM8aAyPfqXZDtq2-rRrMqpJZxBc0";
+const CLI_ID =
+  "8803561872-jd3c7f4e1ugeld0l6ssfmse40n5nfr6l.apps.googleusercontent.com";
+const SCOPES =
+  "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/photoslibrary.readonly"; // Space delimited if more than one
+const DISCOVERY = [
+  "https://sheets.googleapis.com/$discovery/rest?version=v4",
+  "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
+  "https://www.googleapis.com/discovery/v1/apis/photoslibrary/v1/rest",
+];
 
 /**
  * The google libraries are loaded, and ready for action!
@@ -15,7 +16,7 @@ function proceedAsLoaded() {
   if (Goth.recognize()) {
     Goth.onetap();
   } else {
-    gotoTab('Auth')
+    gotoTab("Auth");
     forceSignin();
   }
 }
@@ -24,24 +25,28 @@ function proceedAsLoaded() {
  * They have to correctly get through the button click / sign up flow to proceed.
  */
 function forceSignin() {
-  Goth.button('signin', {type: 'standard', size: 'large', text: 'signup_with'});
+  Goth.button("signin", {
+    type: "standard",
+    size: "large",
+    text: "signup_with",
+  });
 }
 
 function signoutEvent() {
-  document.getElementById('signin').style.display = 'block';
-  gotoTab('Auth')
+  document.getElementById("signin").style.display = "block";
+  gotoTab("Auth");
   forceSignin();
 }
 
 function revokeEvent() {
-  document.getElementById('signin').style.display = 'block';
-  Goth.revoke()
-  gotoTab('Auth')
+  document.getElementById("signin").style.display = "block";
+  Goth.revoke();
+  gotoTab("Auth");
   forceSignin();
 }
 
 function proceedAsSignedIn() {
-  document.getElementById('signin').style.display = 'none';
+  document.getElementById("signin").style.display = "none";
   runApp();
 }
 
@@ -50,20 +55,20 @@ function proceedAsSignedIn() {
  */
 function gothWatch(event) {
   switch (event) {
-    case 'signin':
+    case "signin":
       proceedAsSignedIn();
       break;
-    case 'revoke':
-    case 'signout': 
+    case "revoke":
+    case "signout":
       signoutEvent();
       break;
-    case 'loaded':
+    case "loaded":
       proceedAsLoaded();
       break;
-    case 'onetap_suppressed':
-      forceSignin();  // If a user bypasses onetap flows, we land them with a button.
+    case "onetap_suppressed":
+      forceSignin(); // If a user bypasses onetap flows, we land them with a button.
       break;
-    default: 
+    default:
       console.log("Well, this is a surprise!");
       console.log(event);
   }
@@ -78,45 +83,37 @@ function authorize() {
 }
 
 async function runApp() {
+  user = Goth.user();
 
-  user = Goth.user()
+  var rtn = await getSSId("Travel Companion");
 
-  var rtn = await getSSId('Travel Companion');
-
-  if (rtn.fileId) {spreadsheetId = rtn.fileId}
-  else {
-    await confirm('getSSId error: ' + rtn.msg);
-    window.close()
+  if (rtn.fileId) {
+    spreadsheetId = rtn.fileId;
+  } else {
+    await confirm("getSSId error: " + rtn.msg);
+    window.close();
   }
   await initialUI();
 
-  goHome()
-    
+  goHome();
 }
 
 async function initialUI() {
-  timerStart = new Date()
+  timerStart = new Date();
 
-    arrShts = await openShts(
-      [
-        { title: 'Settings', type: "all" }
-      ])
-    
+  arrShts = await openShts([{ title: "Settings", type: "all" }]);
 
-  console.log('initialUI', arrShts)
+  console.log("initialUI", arrShts);
 
-  arrOptions    = toObject(arrShts.Settings.vals)
-  optionsIdx    = toObjectIdx(arrShts.Settings.vals)
+  arrOptions = toObject(arrShts.Settings.vals);
+  optionsIdx = toObjectIdx(arrShts.Settings.vals);
 
   // disable add/change/delete for Trips and Trip Detail
 
-  if (user['email'] != 'dmoritz10@gmail.com') {
-
-    $('#btnTrpEdit').addClass('d-none')
-    $('#btnTrpAddTrip').addClass('d-none')
-    $('#btnTrpAddNew').addClass('d-none')
-    $('#btnImportPlaces').addClass('d-none')
-    
+  if (user["email"] != "dmoritz10@gmail.com") {
+    $("#btnTrpEdit").addClass("d-none");
+    $("#btnTrpAddTrip").addClass("d-none");
+    $("#btnTrpAddNew").addClass("d-none");
+    $("#btnImportPlaces").addClass("d-none");
   }
-
-};
+}
